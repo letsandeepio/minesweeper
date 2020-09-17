b0dd43f8f4b70827e67251f3aabd4575b4f09688
letsandeepio <sandeepchopra7@gmail.com>
2020-09-16 20:58:46 -0400

    add tests for boardSetup

---
+++ src/helpers/boardSetup.test.js
@@ -0,0 +1,18 @@
+import { boardSetup } from './boardSetup';
+import { initialState } from '../context/reducer';
+
+describe('boardSetip', () => {
+  it('it returns an object', () => {
+    const outputState = boardSetup(initialState);
+    expect(typeof outputState).toEqual('object');
+  });
+
+  it('containing gridArray containing 40 bombs', () => {
+    const outputState = boardSetup(initialState);
+    let bombCount = 0;
+    outputState.gridArray.forEach((row) => {
+      row.forEach((tile) => (bombCount += tile.isBomb ? 1 : 0));
+    });
+    expect(bombCount).toEqual(40);
+  });
+});
